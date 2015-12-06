056adb994da34ee7c4ca4111db73553b62c4c0bb
Hakeem <nasahapps@gmail.com>
2015-12-06 11:41:04 -0800

    Added Utils class

--- NasahUtils.podspec
+++ NasahUtils.podspec
@@ -7,34 +7,35 @@
 #
 
 Pod::Spec.new do |s|
-  s.name             = "NasahUtils"
-  s.version          = "0.1.0"
-  s.summary          = "A short description of NasahUtils."
+s.name             = "NasahUtils"
+s.version          = "0.0.1"
+s.summary          = "Helper methods for everyday Swift life"
 
 # This description is used to generate tags and improve search results.
 #   * Think: What does it do? Why did you write it? What is the focus?
 #   * Try to keep it short, snappy and to the point.
 #   * Write the description between the DESC delimiters below.
-#   * Finally, don't worry about the indent, CocoaPods strips it!  
-  s.description      = <<-DESC
-                       DESC
+#   * Finally, don't worry about the indent, CocoaPods strips it!
+s.description      = <<-DESC
+Just Swift things
+DESC
 
-  s.homepage         = "https://github.com/<GITHUB_USERNAME>/NasahUtils"
-  # s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
-  s.license          = 'MIT'
-  s.author           = { "Hakeem" => "nasahapps@gmail.com" }
-  s.source           = { :git => "https://github.com/<GITHUB_USERNAME>/NasahUtils.git", :tag => s.version.to_s }
-  # s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
+s.homepage         = "https://github.com/nasahapps/SwiftUtils"
+# s.screenshots     = "www.example.com/screenshots_1", "www.example.com/screenshots_2"
+s.license          = { :type => "Apache-2.0", :file => "LICENSE" }
+s.author           = { "Hakeem Hasan" => "nasahapps@gmail.com" }
+s.source           = { :git => "https://github.com/nasahapps/SwiftUtils.git", :tag => s.version.to_s }
+# s.social_media_url = 'https://twitter.com/<TWITTER_USERNAME>'
 
-  s.platform     = :ios, '8.0'
-  s.requires_arc = true
+s.platform     = :ios, '8.0'
+s.requires_arc = true
 
-  s.source_files = 'Pod/Classes/**/*'
-  s.resource_bundles = {
-    'NasahUtils' => ['Pod/Assets/*.png']
-  }
+s.source_files = 'Pod/Classes/**/*'
+s.resource_bundles = {
+'NasahUtils' => ['Pod/Assets/*.png']
+}
 
-  # s.public_header_files = 'Pod/Classes/**/*.h'
-  # s.frameworks = 'UIKit', 'MapKit'
-  # s.dependency 'AFNetworking', '~> 2.3'
+# s.public_header_files = 'Pod/Classes/**/*.h'
+# s.frameworks = 'UIKit', 'MapKit'
+# s.dependency 'AFNetworking', '~> 2.3'
 end
---
+++ Pod/Classes/Utils.swift
@@ -0,0 +1,54 @@
+//
+//  Utils.swift
+//
+//  Created by Hakeem on 8/19/15.
+//  Copyright (c) 2015 Nasah Apps. All rights reserved.
+//
+
+import UIKit
+
+public class Utils {
+    
+    public struct ScreenSize {
+        public static let SCREEN_WIDTH = UIScreen.mainScreen().bounds.width
+        public static let SCREEN_HEIGHT = UIScreen.mainScreen().bounds.height
+        public static let SCREEN_MAX_LENGTH = max(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+        public static let SCREEN_MIN_LENGTH = min(ScreenSize.SCREEN_WIDTH, ScreenSize.SCREEN_HEIGHT)
+        public static let IPHONE_WIDTH_35_4_PORT = CGFloat(320.0)
+        public static let IPHONE_WIDTH_47_PORT = CGFloat(375.0)
+        public static let IPHONE_WIDTH_55_PORT = CGFloat(414.0)
+        public static let IPHONE_WIDTH_35_LAND = CGFloat(480.0)
+        public static let IPHONE_WIDTH_4_LAND = CGFloat(568.0)
+        public static let IPHONE_WIDTH_47_LAND = CGFloat(667.0)
+        public static let IPHONE_WIDTH_55_LAND = CGFloat(736.0)
+        public static let IPAD_MINIMUM_WIDTH = CGFloat(768.0)
+        public static let IPAD_MAXIMUM_WIDTH = CGFloat(1024.0)
+        public static let IPAD_MINIMUM_WIDTH_LARGE = CGFloat(1024.0)
+        public static let IPAD_MAXIMUM_WIDTH_LARGE = CGFloat(1366.0)
+    }
+    
+    public struct DeviceType {
+        public static let IS_PHONE_DEVICE = UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Phone
+        public static let IS_IPHONE_4 = ScreenSize.SCREEN_MAX_LENGTH < 568.0
+        public static let IS_IPHONE_5 = ScreenSize.SCREEN_MAX_LENGTH < 667.0
+        public static let IS_IPHONE_6 = ScreenSize.SCREEN_MAX_LENGTH < 736.0
+        public static let IS_IPHONE_6P = ScreenSize.SCREEN_MAX_LENGTH < 768.0
+        public static let IS_IPAD_DEVICE = UIDevice.currentDevice().userInterfaceIdiom == UIUserInterfaceIdiom.Pad
+        public static let IS_IPAD = ScreenSize.SCREEN_MAX_LENGTH < 1366.0
+        public static let IS_IPAD_PRO = ScreenSize.SCREEN_MAX_LENGTH >= 1366.0
+    }
+    
+    public static func isPortrait(bounds: CGRect) -> Bool {
+        return bounds.width < bounds.height
+        //        return UIDevice.currentDevice().orientation.isPortrait
+        //        return UIDevice.currentDevice().orientation == UIDeviceOrientation.Portrait || UIDevice.currentDevice().orientation == UIDeviceOrientation.PortraitUpsideDown
+    }
+    
+    public static func showAlert(vc: UIViewController, title: String?, message: String?, buttonTitle: String?, action: ((UIAlertAction) -> Void)?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        let defaultAction = UIAlertAction(title: buttonTitle, style: UIAlertActionStyle.Default, handler: action)
+        alert.addAction(defaultAction)
+        vc.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+}
